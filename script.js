b21744e97b1d06bd665b9813f7daac47ac28c8dc
ares <giovaniarias06@gmail.com>
2025-06-10 04:23:47 -0500

    ya se puede agregar tareas

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const inputTarea = document.querySelector(".inputTarea");
 const addTarea = document.querySelector(".addTarea");
+const listaTareas = document.querySelector(".listaTareas");
 
 addTarea.addEventListener("click", () => {
-  let lista = inputTarea.textContent;
+  let valorInput = inputTarea.value;
+  let nuevoLi = document.createElement("li");
+  nuevoLi.classList.add(
+    "text-white",
+    "pl-4",
+    "pt-2",
+    "pb-2",
+    "border-2",
+    "border-solid",
+    "rounded-lg",
+  );
+  nuevoLi.textContent = valorInput;
+  listaTareas.appendChild(nuevoLi);
+  // inputTarea.value = ""; // Limpiar el input después de agregar la tarea
 });
